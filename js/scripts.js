6d1bb056174ff2bae8b0cafa54024999b3030058
Liza Tennis <lizatennis8@gmail.com>
2018-08-17 11:06:37 -0700

    add number range and first foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,40 @@
 $(document).ready(function() {
   $("#questions").submit(function(event) {
 
-    var numberInput = $("#number").val();
+    var numberInput = parseInt($("#number").val());
     var nameInput = $("#name").val();
+    var total = 0;
+    var numberRange = [numberInput, numberInput * 2, numberInput + 10];
+    console.log(numberRange);
 
     $(".name").text(nameInput);
 
-    if (numberInput === "0") {
+    numberRange.forEach(function(element) {
+
+    if (element === 0) {
       $("#beepoutput").show();
-    } else if (numberInput === "1") {
-      $("#boopoutput").show();
-    }
+    } 
+
+    // else if (numberInput === "1") {
+    //   $("#boopoutput").show();
+    // } else if (numberInput === "20") {
+    //   $(index).text();
+    // }
+
+    })
 
     // $("#questions").hide();
     event.preventDefault();
   });
 });
+
+
+
+
+// Tried to loop range but could only get it to show in alert. Couldn't figure out how to push into an array:
+
+// for (var index = 2; index <= numberInput; index += 6) {
+// var range = [];
+// range.push(index);
+// console.log(index);
+  // console.log(range);
