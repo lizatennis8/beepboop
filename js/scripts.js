91a9e00807b90ad0f47183cc3e576ccf5d0237e1
Liza Tennis <lizatennis8@gmail.com>
2018-08-17 15:06:09 -0700

    clean up code and delete unused elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,41 @@
 $(document).ready(function() {
   $("#questions").submit(function(event) {
 
-// for (var index = 2; index <= numberInput; index += 6) {
-// var range = [];
-
     var numberInput = parseInt($("#number").val());
     var nameInput = $("#name").val();
     var total = 0;
-    var numberRange = [numberInput, numberInput * 2, numberInput + 10];
     var endInput = [];
+    var elementArray = [];
 
-    for (var i = 0; i < numberInput; i += 5) {
+    $(".name").text(nameInput);
+
+    for (var i = 0; i <= numberInput; i += 2) {
       var bunchGuys = [i];
       bunchGuys.forEach(function(unit) {
       endInput.push(unit);
       })
     }
 
-console.log(endInput);
-
-    $(".name").text(nameInput);
-
-    var elementArray = [];
-    numberRange.forEach(function(element) {
+    endInput.forEach(function(element) {
     elementArray.push(element);
-
-      })
-// console.log(elementArray);
-
-
-
+    })
 
     elementArray.forEach(function(number) {
       var characters = number.toString().split(" ");
-      // console.log(characters);
-      // console.log(number);
       characters.forEach(function(character) {
 
-      if (character % 3 === 0) {
+      if (number % 3 === 0) {
       $("#pleasework").append(" I'm sorry, " + nameInput + ". I'm afraid I can't do that.");
-    } else if (character.match(/[0]/)) {
-      $("#pleasework").append(" BEEP");
     } else if (character.match(/[1]/)) {
       $("#pleasework").append(" BOOP");
+    } else if (character.match(/[0]/)) {
+      $("#pleasework").append(" BEEP");
     } else {
       $("#pleasework").append(" " + number);
     }
-})
-  });
+      })
+    });
 
-
-  // var digit = number.toString().split('');
-  // console.log(digit);
-
-
-    // $("#questions").hide();
     event.preventDefault();
   });
 });
-
-
-
-
-// Tried to loop range but could only get it to show in alert. Couldn't figure out how to push into an array:
-
-// for (var index = 2; index <= numberInput; index += 6) {
-// var range = [];
-// range.push(index);
-// console.log(index);
-  // console.log(range);
